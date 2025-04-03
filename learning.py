a49fbf02fb8cec04173510e4ca14181cb589a481
brensangura <sangura.bren>
2025-04-04 00:34:00 +0300

    just an update

---
+++ learning.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
+
+print ("3")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+# Personalized Greeting App
+
+# Ask for the user's name
+name = input("What's your name? ")
+
+# Ask for the user's favorite color
+color = input("What's your favorite color? ")
+
+# Print the personalized greeting
+print(f"Hello, {name}! Your favorite color, {color}, is awesome! 👋")
